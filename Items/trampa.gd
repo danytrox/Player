eeb1464b781abc95cd461df2e90007f4a14d08b0
danytrox <120141830+danytrox@users.noreply.github.com>
2024-11-20 16:01:18 -0300

    aa
    aa

--- Items/trampa.gd
+++ Items/trampa.gd
@@ -3,16 +3,12 @@ extends Node2D
 func _ready():
 	pass
 
-
 func _process(delta):
 	pass
-
 
 func _on_area_2d_area_entered(area):
 	if area is HealthComponent:
 		area.set_health(-1)
-
-
 
 func _on_boton_body_entered(body):
 	await get_tree().create_timer(1).timeout
